bf295dd2c3732787fdcdd593630c38b65f93406d
dekbanbanproject <dekbanbanproject@gmail.com>
2023-09-20 23:46:58 +0700

    ok

--- lib/states/admin.dart
+++ lib/states/admin.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pkhos/states/advertise.dart';
+import 'package:pkhos/utility/my_constant.dart';
 import 'package:pkhos/widgets/show_signout.dart';
- 
+import 'package:pkhos/widgets/show_title.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({Key? key}) : super(key: key);
@@ -10,19 +12,174 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
+  Widget currentWidget = AdvertisePage();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.red.shade400,
-        title: Text('ผู้ดูแลระบบ'),
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade400,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Ver.660920',
+              style: MyConstant().h1whit17(),
+            ),
+          ],
+        ),
       ),
+      // drawer: Drawer(
+      //   child: ShowSignOut(),
+      // ),
       drawer: Drawer(
-        child: ShowSignOut(),
+        child: Stack(
+          children: [
+            ShowSignOut(),
+            Column(
+              children: [
+                UserAccountsDrawerHeader(accountName: null, accountEmail: null),
+                buildAdvertisePagemenu(),
+                buildLeavemenu(),
+                buildMeetingroommenu(),
+                buildCarmenu(),
+                buildAssetmenu(),
+                buildSuppliesmenu(),
+                buildWhereHousemenu(),
+              ],
+            ),
+          ],
+        ),
+      ),
+      // body: currentWidget);
+    );
+  }
+
+  ListTile buildAdvertisePagemenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {
+          currentWidget = AdvertisePage();
+          Navigator.pop(context);
+        });
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 30,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'ประชาสัมพันธ์',
+        textStyle: MyConstant().h2dark(),
       ),
     );
   }
 
-  
+  ListTile buildLeavemenu() {
+    return ListTile(
+      //  onPressed: () => Navigator.pushNamed(context, '/reset'),
+      onTap: () {
+        Navigator.pushNamed(context, '/gleave');
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 30,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'ข้อมูลการลา',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
 
-  
+  ListTile buildMeetingroommenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {});
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 30,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'งานบริการห้องประชุม',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
+
+  ListTile buildCarmenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {});
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 30,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'งานบริการยานพาหนะ',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
+
+  ListTile buildAssetmenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {});
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 36,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'งานบริหารทรัพย์สิน',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
+
+  ListTile buildSuppliesmenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {});
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 36,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'งานพัสดุ',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
+
+  ListTile buildWhereHousemenu() {
+    return ListTile(
+      onTap: () {
+        setState(() {});
+      },
+      leading: Icon(
+        Icons.quick_contacts_dialer_outlined,
+        size: 36,
+        color: Colors.orange,
+      ),
+      title: ShowTitle(
+        title: 'คลังวัสดุ',
+        textStyle: MyConstant().h2dark(),
+      ),
+    );
+  }
 }
