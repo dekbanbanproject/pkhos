a5b3ad235278f9f9aa083ead1813a6376bad5f66
dekbanbanproject <dekbanbanproject@gmail.com>
2024-03-06 23:51:02 +0700

    22

--- lib/states/user.dart
+++ lib/states/user.dart
@@ -9,35 +9,73 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+ bool isFABExtended = false; 
+
   @override
+  // void initState() {
+  //   super.initState();    
+  // }
+  // void _switchButton() {
+  //   setState(
+  //     () {
+  //       isFABExtended = !isFABExtended;
+  //     },
+  //   );
+  // }
   Widget build(BuildContext context) {
     // return const Placeholder();
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 249, 232, 240),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 249, 232, 240),
+         backgroundColor: const Color.fromRGBO(82, 170, 94, 1.0),
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            IconButton(
+                onPressed: () {}, 
+                icon: const Icon(Icons.photo_camera_front, color: Color.fromARGB(255, 255, 255, 255)
+                )
+              ),
             Text(
-              'Ver.670305',
+              // 'Ver.670305',
+              'PK-Service',
               style: MyConstant().h1whit17(),
             ),
           ],
         ),
       ),
-     
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.lightBlueAccent,
-        foregroundColor: Color.fromARGB(255, 250, 240, 245),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          // elevation: 12,
+          backgroundColor: const Color.fromRGBO(82, 170, 94, 1.0),
+          tooltip: 'Menu', 
+          child: const Icon(Icons.add,color: Colors.white, size: 28),
+          onPressed: (){},
+          ),
       bottomNavigationBar: BottomAppBar(
-          color: Colors.lightBlue,
-          child: Container(height: 50.0,),
+        shape: const CircularNotchedRectangle(), 
+        notchMargin: 5.0,
+         color: const Color.fromRGBO(82, 170, 94, 1.0),
+          // shape: const CircularNotchedRectangle(), 
+          // shape: cur,
+          // shape: BeveledRectangleBorder(
+              //   borderRadius: BorderRadius.zero
+              // ),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {}, 
+                icon: const Icon(Icons.home, color: Color.fromRGBO(43, 217, 254, 1.0))
+              ),
+              IconButton(
+                onPressed: () {}, 
+                icon: const Icon(Icons.favorite, color: Colors.red
+                )
+              )
+            ],
+          )
       ),
-       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
