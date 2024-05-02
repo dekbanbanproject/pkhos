dae5b4b36f0990453a56ad595da9d6a898ca10ff
dekbanbanproject <dekbanbanproject@gmail.com>
2024-05-03 00:54:43 +0700

    aa

--- api/getArticleupdate.php
+++ api/getArticleupdate.php
@@ -23,7 +23,31 @@
             $camera_save   = $_GET['cctv_camera_save'];
             $power_backup  = $_GET['cctv_camera_power_backup'];
 
-            $sql = "UPDATE `cctv_check` SET `cctv_camera_screen` = '$screen',`cctv_camera_corner` = '$corner',`cctv_camera_drawback` = '$drawback',`cctv_camera_save` = '$camera_save',`cctv_camera_power_backup` = '$power_backup' WHERE cctv_check_id = '$cctv_check_id'";
+            //Qury article_data
+            $sql3 = "SELECT `article_num` FROM cctv_check WHERE cctv_check_id = '$cctv_check_id'";
+            $resline3 = mysqli_query($conn, $sql3) or die ("Error : $sql3" .mysqli_error());
+            while($obje2 = mysqli_fetch_array($resline3,MYSQLI_ASSOC))
+            {
+                $article_num_s       = $obje2["article_num"];
+                // $fire_              = $obje2["fire"]; 
+                // $cctv_type_         = $obje2["cctv_type"]; 
+                // $fire_              = $obje2["fire"]; 
+                // $cctv_location_     = $obje2['cctv_location'];   
+            }
+
+             //Qury article_data
+             $sql2 = "SELECT `article_name`,`cctv_location`,`cctv_type`,`fire` FROM article_data WHERE article_num = '$article_num_s'";
+             $resline2 = mysqli_query($conn, $sql2) or die ("Error : $sql2" .mysqli_error());
+             while($obje = mysqli_fetch_array($resline2,MYSQLI_ASSOC))
+             {
+                 $article_num_       = $obje["article_num"];
+                 $fire_              = $obje["fire"]; 
+                 $cctv_type_         = $obje["cctv_type"]; 
+                 $fire_              = $obje["fire"]; 
+                 $cctv_location_     = $obje['cctv_location'];   
+             }
+
+            $sql = "UPDATE `cctv_check` SET `cctv_camera_screen` = '$screen',`cctv_camera_corner` = '$corner',`cctv_camera_drawback` = '$drawback',`cctv_camera_save` = '$camera_save',`cctv_camera_power_backup` = '$power_backup',`cctv_type` = '$cctv_type_',`cctv_location` = '$cctv_location_' WHERE cctv_check_id = '$cctv_check_id'";
             $result = mysqli_query($conn, $sql) or die ("Error : $sql" .mysqli_error());
             
             // $article_num = $_GET['article_num'];
