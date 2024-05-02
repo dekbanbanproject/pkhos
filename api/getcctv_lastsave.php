6d61193b2356e14366b71d208f9110998523f62f
dekbanbanproject <dekbanbanproject@gmail.com>
2024-05-02 22:36:20 +0700

    dd

--- api/getcctv_lastsave.php
+++ api/getcctv_lastsave.php
@@ -24,24 +24,9 @@
 
             // $article_num = $_GET['article_num'];
 
-            $result = mysqli_query($conn,"SELECT * FROM cctv_check WHERE cctv_check_date BETWEEN '$newDate' AND '$date' AND cctv ='Y' ORDER BY cctv_check_date DESC");
-
-// GROUP BY article_num
-            // $result = mysqli_query($conn,"SELECT a.article_id,c.article_num, a.article_name,a.article_price ,a.article_year,a.article_status_id,a.article_img,a.article_img_name,a.store_id,a.cctv,a.cctv_location\n"
-            //          . ",a.cctv_location_detail,a.cctv_type,a.cctv_code,a.cctv_monitor,a.cctv_status\n"
-            //         . "FROM cctv_check c LEFT JOIN  article_data a ON a.article_num = c.article_num WHERE c.cctv_check_date BETWEEN '$newweek' AND '$date' GROUP BY c.article_num
-            // ");
-
-            // $result = mysqli_query($conn,"SELECT article_id,article_num, article_name,article_price ,article_year,article_recieve_date,article_typeid,article_typename\n"
-            //         . ",article_categoryid,article_categoryname,article_groupid,article_groupname,article_status_id,article_img,article_img_name,store_id,cctv,cctv_location\n"
-            //         . ",cctv_location_detail,cctv_type,cctv_code,cctv_monitor,cctv_status\n"
-            //         . "FROM article_data WHERE cctv ='Y' AND article_num = '$article_num'
-            // ");
-            // SELECT * FROM cctv_check
-          
-            // $data = $result->mysqli_fetch_assoc();
-            // echo json_encode($data);
-            
+            $result = mysqli_query($conn,"SELECT * FROM cctv_check WHERE cctv_check_date BETWEEN '$newDate' AND '$date' AND cctv='Y' ORDER BY cctv_check_date DESC
+            ");
+ 
             if ($result) {
                 while($row=mysqli_fetch_assoc($result)){
                     $output[]=$row;
